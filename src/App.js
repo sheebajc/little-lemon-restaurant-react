26321865b2fcf3616e16a86b613c7feef0f2d118
Sheeba <sheebachrisjason@gmail.com>
2024-10-21 12:48:01 +0530

    Little lemon

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import RootLayout from './pages/RootLayout';
+import Home from './pages/Home';
+import Booking from './pages/Booking';
+import Confirmation from './pages/Confirmation';
+import { fetchAPI, submitAPI } from './LiitleLemonResBookingAPI';
 
 function App() {
+
+  const initialState = { availableTimes: fetchAPI(new Date()) }
+  const [state, dispatch] = React.useReducer(updateTimes, initialState);
+
+  function updateTimes(state, date) {
+    return { availableTimes: fetchAPI(new Date(date)) }
+  }
+  function submitForm(formData) {
+    if (submitAPI(formData)) {
+    }
+  }
+
+  const router = createBrowserRouter([{
+    path: '',
+    element: <RootLayout />,
+    children: [{
+      path: '',
+      element: <Home />
+    },
+    {
+      path: 'booking',
+      element: <Booking availableTimes={state} dispatch={dispatch} submitForm={submitForm}/>
+    },
+    {
+      path: 'confirmation',
+      element: <Confirmation />
+    }
+    ]
+  }])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router} />
+
   );
 }
 
